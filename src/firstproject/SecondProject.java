760e2c6d7593a9098e72188da96973570ffd308d
Pasupathy Muthupandy <pasupathymuthupandy@Pasupathys-iMac.lan>
2021-11-05 11:15:09 -0500

    Data type and convertion

---
+++ src/firstproject/SecondProject.java
@@ -0,0 +1,51 @@
+package firstproject;
+
+
+public class SecondProject {
+    public static void main(String[] args) {
+
+    add();
+
+    }
+
+        public static void  add() {
+            int num1 =5;
+            int num2 =18;
+            int num3 =23;
+            int num4 =-13;
+int result = (num1 + num2 ) - (num1 -num4)* num2 +num3 /num1 ;
+            System.out.println(result);
+
+double tempFah = 55;
+double tempCwl= (tempFah-32)*5/9;
+            System.out.println("converting fahr to celcius:" +tempCwl);
+int a1 = 40;
+int b1= 3;
+int c1 = a1%b1;
+            System.out.println("modulus operator " +c1);
+
+            double b = 4000;
+            int a  = 30;
+            double c = b/a;
+            System.out.println(c);
+
+            System.out.println("\nhi");
+
+String siva = "sankari";
+String hell = siva + " mad";
+            System.out.println(" my name is = "+ hell);
+
+int inc=5;
+inc++;
+            System.out.println("value of 1inc++: "+inc);
+            ++inc;
+            System.out.println("value of 2inc++: "+inc);
+            inc++;
+            System.out.println("value of 3inc++: "+inc);
+            ++inc;
+            System.out.println("value of 4inc++: "+inc);
+            }
+        }
+
+
+
