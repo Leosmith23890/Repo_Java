54c194ee01b3188d7f3fc2aa4982666503e53285
Pasupathy Muthupandy <pasupathymuthupandy@Pasupathys-iMac.lan>
2021-11-08 00:52:46 -0600

    Strings Homework

--- src/firstproject/SecondProject.java
+++ src/firstproject/SecondProject.java
@@ -1,14 +1,10 @@
 package firstproject;
 
 
+
 public class SecondProject {
     public static void main(String[] args) {
 
-    add();
-
-    }
-
-        public static void  add() {
             int num1 =5;
             int num2 =18;
             int num3 =23;
@@ -19,31 +15,36 @@ int result = (num1 + num2 ) - (num1 -num4)* num2 +num3 /num1 ;
 double tempFah = 55;
 double tempCwl= (tempFah-32)*5/9;
             System.out.println("converting fahr to celcius:" +tempCwl);
+
 int a1 = 40;
 int b1= 3;
 int c1 = a1%b1;
-            System.out.println("modulus operator " +c1);
+System.out.println("modulus operator " +c1);
 
-            double b = 4000;
-            int a  = 30;
-            double c = b/a;
-            System.out.println(c);
+double b = 4000;
+int a  = 30;
+double c = b/a;
+System.out.println(c);
 
-            System.out.println("\nhi");
+System.out.println("\nhi");
 
 String siva = "sankari";
-String hell = siva + " mad";
-            System.out.println(" my name is = "+ hell);
+String hello = siva + " berry";
+System.out.println(" my name is = "+ hello) ;
+String s ="siva";
+String t="SIVA";
+boolean o = s.contains("IV");
+System.out.println(0);
+boolean y=s.toUpperCase().contains(t.toUpperCase());
+String rep="India rocks";
+int j=rep.indexOf("i");
+int j1=rep.indexOf("rocks");
+System.out.println("string" +j);
+System.out.println("string" +j1);
+System.out.println(y);
 
-int inc=5;
-inc++;
-            System.out.println("value of 1inc++: "+inc);
-            ++inc;
-            System.out.println("value of 2inc++: "+inc);
-            inc++;
-            System.out.println("value of 3inc++: "+inc);
-            ++inc;
-            System.out.println("value of 4inc++: "+inc);
+
+
             }
         }
 
