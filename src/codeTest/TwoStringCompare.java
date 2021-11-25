b89c80cbf50c9aaf319ba0a3d5a7d308b0880f9d
Leosmith23890 <93276857+Leosmith23890@users.noreply.github.com>
2021-11-25 02:25:44 -0600

    Update TwoStringCompare.java

--- src/codeTest/TwoStringCompare.java
+++ src/codeTest/TwoStringCompare.java
@@ -11,24 +11,16 @@ public class TwoStringCompare {
         String a2S = Arrays.toString(a2);
         System.out.println(a1S);
         System.out.println(a2S);
-        String convert1 = a1S.toLowerCase();
-        String convert2 = a2S.toLowerCase();
-        System.out.println("after conversion" + convert1);
-        System.out.println("after conversion" + convert2);
-        String[] spl1 = convert1.split(",");
-        String AfterSplit1 = Arrays.toString(spl1);
-        String[] spl2 = convert2.split(", ");
-        String AfterSplit2 = Arrays.toString(spl2);
 
-        for (int i = 0; i < spl1.length-1; i++){
-            for (int j = 0; j < spl2.length-1; j++){
-              if(spl1[i].contains(spl2[j])) {
-                    System.out.println(spl1[i]);
-
-
+       for (int i = 0; i < a1.length; i++) {
+            for (int j = 0; j < a2.length; j++) {// inner loop
+                if (a1[i].equalsIgnoreCase(a2[j])) {
+                    System.out.println(a1[i]);
+                 
                 }
-
-        }}}}
-
+            }
+        }
+    }
+}
 
 
